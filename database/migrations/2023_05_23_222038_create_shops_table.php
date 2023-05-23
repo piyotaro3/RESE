fffd58fd2d9579a5d2e207d15f48bb72bfee9722
piyotaro3 <yudengyuanshang@gmail.com>
2023-05-24 00:08:45 +0900

    マイグレーションの実装

---
+++ database/migrations/2023_05_23_222038_create_shops_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->nullable(false);
+            $table->integer('genre_id')->unsigned()->nullable(false);
+            $table->integer('area_id')->unsigned()->nullable(false);
+            $table->mediumText('detail')->nullable(false);
+            $table->string('image', 255)->nullable(false);
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
