23fdc1b03f48a70d81c899d0ebe8ab9d0c3cee90
piyotaro3 <yudengyuanshang@gmail.com>
2023-06-01 22:43:24 +0900

    Revert "Revert "thanksページ""
    This reverts commit b9e28b2e0196b7b90ffe7c18b33d6912607ef1fa.

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class RegisteredUserController extends Controller
 {
-    public function index()
+    public function create()
     {
-       return view('register');
+        return view('register');
     }
 
-    public function thanks()
+
+    public function store(Request $request)
     {
-        return view('thanks');
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect('/thanks');
     }
+
 }
