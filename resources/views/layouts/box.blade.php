047bef7c24b179da7e58abc669123fe961085871
piyotaro3 <yudengyuanshang@gmail.com>
2023-06-07 17:00:41 +0900

    不要ファイル削除（layouts）

--- resources/views/layouts/box.blade.php
+++
@@ -1,8 +0,0 @@
-@section('box')
-    <div class="message_area">
-        <div class="message_box">
-            <p class="thanks">@yield('message')</p>
-                <a href="@yield('url')" class="login">@yield('action')</a>
-        </div>
-    </div>
-@endsection
