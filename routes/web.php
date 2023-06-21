5f203708f5bd44542785c085d86de42d9db64c4c
piyotaro3 <yudengyuanshang@gmail.com>
2023-06-21 21:26:44 +0900

    お気に入り機能の認証を実装

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,10 @@ Route::get('/', [ShopController::class, 'index'])->name('shop.index');
 Route::get('/search', [ShopController::class, 'search'])->name('shop.search');
 
 /**お気に入り機能 */
-Route::post('/shop/{shop}favorite', [FavoriteController::class, 'create'])->name('favorite.create');
-Route::post('/shop/{shop}/unfavorite', [FavoriteController::class, 'delete'])->name('favorite.delete');
+Route::group(['middleware' => 'auth'], function () {
+    Route::post('/shop/{shop}favorite', [FavoriteController::class, 'create'])->name('favorite.create');
+    Route::post('/shop/{shop}/unfavorite', [FavoriteController::class, 'delete'])->name('favorite.delete');
+});
 
 /**会員登録完了 */
 Route::get('/thanks', function () {
