3dfb5f5db63b7719a89f4a6264f771a32e814207
piyotaro3 <yudengyuanshang@gmail.com>
2023-06-26 19:08:42 +0900

    店舗詳細の作成

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,9 +5,27 @@
 @endsection
 
 @section('content')
-    @foreach ($shops as $shop)
-        <p>店舗名{{ $shop->name }}</p>
-        <p>{{ $shop->getarea() }}{{ $shop->getGenre() }}</p>
-        <p>{{ $shop->detail }}</p>
-    @endforeach
+    <div class="content">
+        @foreach ($shops as $shop)
+            <div class="shop_detail">
+                <div class="title_area">
+                    <a href="/">
+                        <div class="back_btn">
+                            <img
+                                src="{{ asset('img/前に戻るアイコン4.png') }}" class="test" >
+                        </div>
+                    </a>
+                    <h2 class="shop_title">{{ $shop->name }}</h2>
+                </div>
+                <div class="shop_img">
+                    <img src="{{ asset($shop->image) }}">
+                </div>
+                <div class="tags">
+                    <p>{{ $shop->getarea() }}{{ $shop->getGenre() }}</p>
+                </div>
+                <div class="detail">
+                    <p>{{ $shop->detail }}</p>
+                </div>
+        @endforeach
+    </div>
 @endsection
