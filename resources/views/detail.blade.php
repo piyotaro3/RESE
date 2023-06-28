b52f037565b67e175666f8c285197a565522849d
piyotaro3 <yudengyuanshang@gmail.com>
2023-06-28 18:22:56 +0900

    詳細画面

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -11,8 +11,7 @@
                 <div class="title_area">
                     <a href="/">
                         <div class="back_btn">
-                            <img
-                                src="{{ asset('img/前に戻るアイコン4.png') }}" class="test" >
+                            <img src="{{ asset('img/前に戻るアイコン4.png') }}" class="back_img">
                         </div>
                     </a>
                     <h2 class="shop_title">{{ $shop->name }}</h2>
@@ -26,6 +25,46 @@
                 <div class="detail">
                     <p>{{ $shop->detail }}</p>
                 </div>
+            </div>
         @endforeach
+        <div class="test">
+            <div>
+                <h2 class="title_reserve">予約</h2>
+            </div>
+            <form action="/reserve" method="POST">
+                @csrf
+                <div class="reserve_day">
+                    <input type="date" id="today">
+                </div>
+                <div class="reserve_time">
+                    <input type="time" step="1800">
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
+
+{{-- 今日の日付 --}}
+<script type="text/javascript">
+    window.onload = function() {
+        var date = new Date()
+        var year = date.getFullYear()
+        var month = date.getMonth() + 1
+        var day = date.getDate()
+
+        var toTwoDigits = function(num, digit) {
+            num += ''
+            if (num.length < digit) {
+                num = '0' + num
+            }
+            return num
+        }
+
+        var yyyy = toTwoDigits(year, 4)
+        var mm = toTwoDigits(month, 2)
+        var dd = toTwoDigits(day, 2)
+        var ymd = yyyy + "-" + mm + "-" + dd;
+
+        document.getElementById("today").value = ymd;
+    }
+</script>
