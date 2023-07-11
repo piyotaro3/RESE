5c25725d3fa407cb110b44ad3b93b867a59a2509
piyotaro3 <yudengyuanshang@gmail.com>
2023-07-11 22:10:55 +0900

    登録完了のデザイン調整完了

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -11,8 +11,8 @@
 @section('content')
     <div class="message_area">
         <div class="message_box">
-            <p class="thanks">会員登録ありがとうございます</p>
-            <a href="/login" class="login">ログインする</a>
+            <p class="message">会員登録ありがとうございます</p>
+            <a href="/login" class="root">ログインする</a>
         </div>
     </div>
 @endsection
