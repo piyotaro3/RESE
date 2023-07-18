5da6312f2f3c928619da3dc503e1938bc0c6e88b
piyotaro3 <yudengyuanshang@gmail.com>
2023-07-18 17:38:26 +0900

    会員登録関連不要分削除と基本設計書の確認

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,44 +4,24 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 use App\Http\Requests\RegisterRequest;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display the registration view.
-     *
-     * @return \Illuminate\View\View
-     */
     public function create(Request $request)
     {
         return view('auth.register');
     }
 
-    /**
-     * Handle an incoming registration request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
     public function store(RegisterRequest $request)
     {
-        $user = $request->validated();
-
-        $user = User::create([
+        User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
         return redirect('/thanks');
     }
 }
--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -6,42 +6,17 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-            'name' => 'required | string | max:255',
-            'email' => 'required | email | string | max:255 | unique:users',
-            'password' => 'required | min:8 | max:255',
-        ];
-    }
-    public function messages()
-    {
-        return [
-            'name.required' => '名前を入力してください',
-            'name.max:255' => '文字数は255以内で入力してください',
-            'name.string' => '文字型で入力してください',
-            'email.required' => 'メールアドレスを入力してください',
-            'email.email' => 'メールアドレス形式で入力してください',
-            'email.string' => '文字型で入力してください',
-            'email.max:255' => '255以内で入力してください',
-            'password.required' => 'パスワードを入力してください',
-            'password.min:8' => 'パスワードは８文字以上入力してください',
-            'password.max:255' => 'パスワードは２５５文字以内で入力してください',
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|string|max:255|unique:users',
+            'password' => 'required|min:8|max:255',
         ];
     }
 }
