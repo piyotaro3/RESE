5082ebf252e8122e0a3826c2bd06183053b68587
piyotaro3 <yudengyuanshang@gmail.com>
2023-07-18 16:50:56 +0900

    ShopControllerの確認　インデントの修正

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -8,13 +8,10 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
 use App\Models\Area;
 use App\Models\Genre;
-use App\Models\User;
-use App\Models\Favorite;
 use App\Models\Reserve;
 
 class ShopController extends Controller
 {
-    /**店舗一覧表示 */
     public function index()
     {
         $user = Auth::user();
@@ -31,7 +28,6 @@ class ShopController extends Controller
         return view('shop', $param);
     }
 
-    /**店舗検索 */
     public function search(Request $request)
     {
         $user = Auth::user();
@@ -48,8 +44,8 @@ class ShopController extends Controller
             $query->where('area_id', $area_id);
         if ($word != null)
             $query->where('name', 'LIKE BINARY', "%$word%");
-
         $shops = $query->get();
+
         $param = [
             'shops' => $shops,
             'user' => $user,
@@ -61,7 +57,6 @@ class ShopController extends Controller
         return view('shop', $param);
     }
 
-    /**店舗詳細表示 */
     public function detail(Request $request)
     {
         $user = Auth::user();
@@ -72,12 +67,10 @@ class ShopController extends Controller
         $query = Shop::query();
         if ($shop_id != null)
             $query->where('id', $shop_id);
-
         $shops = $query->get();
 
-        /** 最新の予約情報取得 */
-        $check = Reserve::where('user_id', $user_id)->where('shop_id', $shop_id)->exists(); /**カラムが存在するか確認 */
-        $reserves = Reserve::where('user_id', '=', $user_id)->where('shop_id', '=', $shop_id)->orderBy('id', 'desc')->take(1)->get(); /**最新のカラムを1件取得 */
+        $check = Reserve::where('user_id', $user_id)->where('shop_id', $shop_id)->exists();
+        $reserves = Reserve::where('user_id', '=', $user_id)->where('shop_id', '=', $shop_id)->orderBy('id', 'desc')->take(1)->get();
 
         $param = [
             'shops' => $shops,
@@ -85,7 +78,6 @@ class ShopController extends Controller
             'reserves' => $reserves,
             'check' => $check,
         ];
-       
         return view('detail', $param);
     }
 }
