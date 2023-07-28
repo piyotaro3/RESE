5bfbd5e1a7f0a32ff5af9681b7da1f1d9b159731
piyotaro3 <yudengyuanshang@gmail.com>
2023-07-29 00:58:13 +0900

    予約一覧の修正（今日以前の予約を取得しない）

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class MypageController extends Controller
 {
     public function show()
     {
+        $today = Carbon::today();
         $user = Auth::user();
         $id = auth()->id();
-        $reserves = User::find($id)->reserve_shop()->orderBy('day', 'asc')->get();
+        $reserves = User::find($id)->reserve_shop()->whereDate('day', '>=', $today)->orderBy('day', 'asc')->get();
         $favorites = User::find($id)->favorite_shop()->get();
 
         $param = [
